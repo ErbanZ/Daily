f6f62da4781b52a6765f043882bf8ba678ce1795
zlj-R7000P <zlyj365@qq.com>
2022-04-26 23:55:09 +0800

    py

---
+++ daily-python/april/0425.py
@@ -0,0 +1,38 @@
+'''
+Date: 2022-04-25 20:14:32
+LastEditors: r7000p
+LastEditTime: 2022-04-26 23:54:25
+FilePath: \Daily\daily-python\april\0425.py
+'''
+
+matrix3 = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+def rotate90(matrix):
+    # n = len(matrix)
+    # for i in range(n):
+    #     for j in range(i):
+    #         temp = matrix[i][j]
+    #         matrix[i][j] = matrix[j][i]
+    #         matrix[j][i] = temp
+    
+    # for i in range(n):
+    #     for j in range(n // 2):
+    #         temp = matrix[i][j]
+    #         matrix[i][j] = matrix[i][n-j-1]
+    #         matrix[i][n-j-1] = temp
+
+    # 短链接服务：
+    # 流程：crm后台触发包含短链接的短信→用户收到包含短链接的短信→用户点击该短链接→跳转到目标域名和页面
+    # 如何校验替换成功？
+    # 若采用过期的证书，表现如何？未过期的呢？
+    # 在主流机型的原生手机（）浏览器、主流手机浏览器进行操作
+    # 手机：iphone、华为（荣耀）、小米、ov、1+、三星
+    # 浏览器：手机原生、微信自带、百度、UC、QQ、夸克、搜狗、360火狐、via
+
+    return matrix
+
+print(rotate90(matrix3))
--- daily-web/web-project/my-site02/scripts/script.js
+++ daily-web/web-project/my-site02/scripts/script.js
@@ -1,2 +1,9 @@
-let headname = document.querySelector("h1")
-headname.textContent("Debug The World!")
+/*
+ * @Date: 2022-04-25 20:02:48
+ * @LastEditors: r7000p
+ * @LastEditTime: 2022-04-25 20:08:17
+ * @FilePath: \Daily\daily-web\web-project\my-site02\scripts\script.js
+ */
+
+let headname = document.querySelector('h1')
+headname.textContent = "Debug The World!"
