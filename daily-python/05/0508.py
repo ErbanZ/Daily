96dd27f31da674131adb3901f0bddadea3ee725c
zlj-R7000P <zlyj365@qq.com>
2022-05-10 23:45:09 +0800

    0510

--- daily-python/05/0508.py
+++ daily-python/05/0508.py
@@ -1,10 +1,13 @@
 # 字符串匹配算法：KMP
 # https://leetcode-cn.com/leetbook/read/array-and-string/cpoo6/
 
-s = "AAAAABBAAG"
-t = "ABBACG"
+s = "ababababca"
+s1 = "aaaaaaaab"
+t = "abababca"
+t1 = "aaaaaab"
 
-def matchstring(s1, s2):
+
+def simplematchstring(s1, s2):
     i = 0
     while i <= len(s1) - len(s2):
         idx = i
@@ -18,6 +21,34 @@ def matchstring(s1, s2):
             return True
         i += 1
     return False
-                
+
+def getPMT(s):
+    res = [0] * len(s)
+    i, j = 1, 0
+    while i < len(s):
+        if s[i] == s[j]:
+            res[i] = j + 1
+            i += 1
+            j += 1
+        else:
+            j = res[i]
+            i += 1
+    return res
+
+def KMP(s, t):
+    tn = getPMT(t)
+    i, j = 0, 0
+    while j < len(t):
+        if s[i] == t[j]:
+            i += 1
+            j += 1
+        else:
+            j = tn[j-1]
+    if j == len(t):
+        return True
+    return False        
+
 if __name__ == "__main__":
-    print(matchstring(s, t))
+    # print(simplematchstring(s, t))
+    print(KMP(s, t))
+    print(KMP(s1, t1))
