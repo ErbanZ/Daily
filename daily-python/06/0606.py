54cc15d40362193e35df8663098438ccfcdbe1c3
zlj-R7000P <zlyj365@qq.com>
2022-06-07 21:31:54 +0800

    0606

---
+++ daily-python/06/0606.py
@@ -0,0 +1,9 @@
+'''
+Date: 2022-06-06 20:19:43
+LastEditors: r7000p
+LastEditTime: 2022-06-06 20:19:59
+FilePath: \Daily\daily-python\06\0606.py
+'''
+_list = [None] * 5
+
+print(_list)
--- daily-web/web-project/my-site02/scripts/script.js
+++
@@ -1,9 +0,0 @@
-/*
- * @Date: 2022-04-25 20:02:48
- * @LastEditors: r7000p
- * @LastEditTime: 2022-04-25 20:08:17
- * @FilePath: \Daily\daily-web\web-project\my-site02\scripts\script.js
- */
-
-let headname = document.querySelector('h1')
-headname.textContent = "Debug The World!"
