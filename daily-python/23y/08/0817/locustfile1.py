f78bc82f9e785f90eeb39774b4dc7244af21335b
max.zhang(张龙九) <max.zhang@xgimi.com>
2023-08-18 10:34:15 +0800

    0818

---
+++ daily-python/23y/08/0817/locustfile1.py
@@ -0,0 +1,18 @@
+from locust import HttpUser, task
+import requests
+
+class hello(HttpUser):
+    host = "https://httpbin.org"
+
+    # @task
+    # def hello_get(self):
+    #     self.client.get("/get")
+
+    # @task
+    # def hello_post(self):
+    #     self.client.post("/post")
+
+    @task
+    def hello_name(self):
+        for i in range(10):
+            self.client.get(f"/get?id={i}", name="/get?id=666")
