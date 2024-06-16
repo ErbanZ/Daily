677dc3c0df8aaa4480f902432afcb63d3a84a188
zlj-R7000P <zlyj365@qq.com>
2024-06-16 15:06:25 +0800

    20240616

---
+++ daily-python/24y/pytest_dir/Common/Calculator.py
@@ -0,0 +1,22 @@
+# coding=utf-8
+
+# Description: The file description here
+import logging
+
+
+class Calculator:
+    @staticmethod
+    def add(a, b):
+        return a + b
+
+    @staticmethod
+    def subtract(a, b):
+        return a - b
+
+
+def main():
+    print('----------')
+
+
+if __name__ == '__main__':
+    main()
